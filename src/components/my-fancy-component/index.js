7632569b0da628134aa413eeb5cd343efe54b136
Tim Reen <timothy.reen@gmail.com>
2019-05-15 13:32:44 -0400

    add alert

--- src/components/my-fancy-component/index.js
+++ src/components/my-fancy-component/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 
-const MyFancyComponent = () => <button>Click Here to Order Food</button>;
+const MyFancyComponent = () => {
+  const fireAlert = () => window.alert('Food is on the way');
+
+  return <button onClick={fireAlert}>Click Here to Order Food</button>
+}
 
 export default MyFancyComponent;
